a04d98705137759e6eea65c99b4df90fec62f0d8
KLAMDI <crunch@hetnet.nl>
2019-12-24 17:20:30 +0100

    Initial commit; created mcmod and main class

---
+++ main/java/com/yzarc/boss/Boss.java
@@ -0,0 +1,20 @@
+package com.yzarc.boss;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Boss.MODID, version = Boss.VERSION, dependencies = "required-after:(props)")
+public class Boss
+{
+    public static final String MODID = "funcocraft";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
