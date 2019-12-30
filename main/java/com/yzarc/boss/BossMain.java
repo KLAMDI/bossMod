fa88857e47905db9264f8ec77ecc193d80fd7b03
KLAMDI <crunch@hetnet.nl>
2019-12-30 14:43:11 +0100

    Added proxies to main class

--- main/java/com/yzarc/boss/BossMain.java
+++ main/java/com/yzarc/boss/BossMain.java
@@ -6,23 +6,34 @@ import com.yzarc.boss.blocks.BossBlocks;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = BossMain.MODID, version = BossMain.VERSION)
+
 public class BossMain
 {
     public static final String MODID = "boss";
     public static final String VERSION = "0.1";
     
+    @SidedProxy(clientSide="com.bedrockminer.tutorial.ClientProxy", serverSide="com.bedrockminer.tutorial.ServerProxy")
+    public static CommonProxy proxy;
+    
     @EventHandler
     public void preinit(FMLInitializationEvent event)
     {
-    	BossBlocks.init();
+    	proxy.preInit(event);
     }
+    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    	proxy.Init(event);
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) 
+    {
+    	proxy.postInit(event);
     }
 }
