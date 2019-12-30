6c2b517f561a234bc417dfa3b47d7b908165667c
steef23 <59206556+steef23@users.noreply.github.com>
2019-12-30 22:38:37 +0100

    Create BossEntities.java
    Added class to initialize our entities into the game

---
+++ main/java/com/yzarc/boss/entity/BossEntities.java
@@ -0,0 +1,36 @@
+package com.yzarc.boss.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
+
+import com.yzarc.boss.BossMain;
+
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class BossEntities 
+{
+	public static EntityBlob blob;
+	
+	public static final void init()
+	{
+		createEntity(EntityBlob.class, "Test Blob", 0x6fd699 , 0x326351);
+	}
+	
+	public static void createEntity(Class entityClass, String entityName, int solidColor, int spotColor)
+	{
+		int randomId = EntityRegistry.findGlobalUniqueEntityId();
+		EntityRegistry.registerGlobalEntityID(entityClass, entityName, randomId);
+		EntityRegistry.registerModEntity(entityClass, entityName, randomId, BossMain.modInstance, 64, 1, true);
+		EntityRegistry.addSpawn(entityClass, 2, 0, 1, EnumCreatureType.creature, BiomeGenBase.plains);
+		
+		createEgg(randomId, solidColor, spotColor);
+	}
+	
+	private static void createEgg(int randomId, int solidColor, int spotColor)
+	{
+		EntityList.entityEggs.put(Integer.valueOf(randomId), new EntityList.EntityEggInfo(randomId, solidColor, spotColor));
+	}
+}
