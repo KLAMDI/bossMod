9298e37c186ad702f6fb398edf06fca254084dce
steef23 <59206556+steef23@users.noreply.github.com>
2019-12-30 15:41:41 +0100

    Update BossBlocks.java
    added more variables to test block

--- main/java/com/yzarc/boss/blocks/BossBlocks.java
+++ main/java/com/yzarc/boss/blocks/BossBlocks.java
@@ -3,13 +3,14 @@ package com.yzarc.boss.blocks;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 
 public class BossBlocks 
 {
     public static Block test;
     public static final void init() 
     {
-    	GameRegistry.registerBlock(test = new CreateBlocks("test", Material.cake, 10.0F, 1.0F), "test_block");
+    	GameRegistry.registerBlock(test = new CreateBlocks("test", Material.cake, CreativeTabs.tabBlock , 10.0F, 6.0F, 1.0F, "pickaxe", 3, Block.soundTypeGravel), "test_block");
     }
     
 }
