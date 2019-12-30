5334256d9304591429bdb64b0f4aeb4709a80eed
KLAMDI <crunch@hetnet.nl>
2019-12-30 13:40:09 +0100

    Changed main class name to BossMain

---
+++ main/java/com/yzarc/boss/BossMain.java
@@ -0,0 +1,20 @@
+package com.yzarc.boss;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BossMain.MODID, version = BossMain.VERSION)
+public class BossMain
+{
+    public static final String MODID = "boss";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
