fa4ce2b73c0d5ed48addeed6fd9253715602996e
steef23 <59206556+steef23@users.noreply.github.com>
2019-12-30 22:39:04 +0100

    Update ClientProxy.java
    deleted whiteline

--- main/java/com/yzarc/boss/ClientProxy.java
+++ main/java/com/yzarc/boss/ClientProxy.java
@@ -6,7 +6,6 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy 
 {
-
 	@Override
 	public void preInit(FMLPreInitializationEvent e) {
 		// TODO Auto-generated method stub
