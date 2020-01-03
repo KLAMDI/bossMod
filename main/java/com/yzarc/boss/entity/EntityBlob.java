3139642df9f5b976d0bf6d479db825fd1b5075eb
steef23 <59206556+steef23@users.noreply.github.com>
2020-01-03 13:34:20 +0100

    Update EntityBlob.java
    Added basic attack and movement AI to blob

--- main/java/com/yzarc/boss/entity/EntityBlob.java
+++ main/java/com/yzarc/boss/entity/EntityBlob.java
@@ -1,8 +1,11 @@
 package com.yzarc.boss.entity;
 
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
@@ -14,6 +17,8 @@ public class EntityBlob extends EntityMob
 		super(worldIn);
 		this.setSize(1.0F, 1.0F);
 		this.tasks.addTask(0, new EntityAIWander(this, 0.5D));
+		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
 		
 		// TODO Auto-generated constructor stub
 	}
@@ -22,11 +27,11 @@ public class EntityBlob extends EntityMob
 	{
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(20.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(10.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.1D);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(5.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(2.0D);
 	}
 	
-	/*public void onLivingUpdate()
+	public void onLivingUpdate()
 	{
 		super.onLivingUpdate();
 	}
@@ -40,6 +45,5 @@ public class EntityBlob extends EntityMob
 	{
 		return super.getCanSpawnHere();
 	}
-	*/
 
 }
