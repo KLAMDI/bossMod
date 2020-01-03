21a17a762b569cc95c85f8ef190a75b3aa68f0bf
KLAMDI <crunch@hetnet.nl>
2020-01-03 13:35:56 +0100

    Now renders blob entity

--- main/java/com/yzarc/boss/ClientProxy.java
+++ main/java/com/yzarc/boss/ClientProxy.java
@@ -1,5 +1,10 @@
 package com.yzarc.boss;
 
+import com.yzarc.boss.entity.EntityBlob;
+import com.yzarc.boss.entity.ModelBlob;
+import com.yzarc.boss.entity.RenderBlob;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -10,6 +15,7 @@ public class ClientProxy extends CommonProxy
 	public void preInit(FMLPreInitializationEvent e) {
 		// TODO Auto-generated method stub
 		super.preInit(e);
+		RenderingRegistry.registerEntityRenderingHandler(EntityBlob.class, new RenderBlob(new ModelBlob(), 0.5F));
 	}
 
 	@Override
