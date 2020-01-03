e0229c70d01847cba59f2694de2394e1d490c7d0
KLAMDI <crunch@hetnet.nl>
2020-01-03 15:18:47 +0100

    Model now has 2 parts that stick out and move in idle, faster when entity moves

--- main/java/com/yzarc/boss/entity/ModelBlob.java
+++ main/java/com/yzarc/boss/entity/ModelBlob.java
@@ -5,11 +5,14 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
 
 @SideOnly(Side.CLIENT)
 public class ModelBlob extends ModelBase {
 	public ModelRenderer core;
+	public ModelRenderer coreLCenter;
+	public ModelRenderer coreRCenter;
 //	private ModelRenderer field_78133_b;
 //	private ModelRenderer body;
 //	private ModelRenderer leg1;
@@ -22,44 +25,29 @@ public class ModelBlob extends ModelBase {
         this(0.0F);
     }
 
-    public ModelBlob(float p_i1147_1_)
+    public ModelBlob(float scale)
     {
         byte b0 = 4;
         this.core = new ModelRenderer(this, 0, 0);
-        this.core.addBox(0.0F, 16.0F, 0.0F, 8, 1, 8, p_i1147_1_);
+        this.core.addBox(0.0F, 19.0F, 0.0F, 8, 1, 8, scale);
         this.core.setRotationPoint(0.0F, (float)b0, 0.0F);
-//        this.field_78133_b = new ModelRenderer(this, 32, 0);
-//        this.field_78133_b.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, p_i1147_1_ + 0.5F);
-//        this.field_78133_b.setRotationPoint(0.0F, (float)b0, 0.0F);
-//        this.body = new ModelRenderer(this, 16, 16);
-//        this.body.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, p_i1147_1_);
-//        this.body.setRotationPoint(0.0F, (float)b0, 0.0F);
-//        this.leg1 = new ModelRenderer(this, 0, 16);
-//        this.leg1.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, p_i1147_1_);
-//        this.leg1.setRotationPoint(-2.0F, (float)(12 + b0), 4.0F);
-//        this.leg2 = new ModelRenderer(this, 0, 16);
-//        this.leg2.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, p_i1147_1_);
-//        this.leg2.setRotationPoint(2.0F, (float)(12 + b0), 4.0F);
-//        this.leg3 = new ModelRenderer(this, 0, 16);
-//        this.leg3.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, p_i1147_1_);
-//        this.leg3.setRotationPoint(-2.0F, (float)(12 + b0), -4.0F);
-//        this.leg4 = new ModelRenderer(this, 0, 16);
-//        this.leg4.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, p_i1147_1_);
-//        this.leg4.setRotationPoint(2.0F, (float)(12 + b0), -4.0F);
+        this.coreLCenter = new ModelRenderer(this, 0, 0);
+        this.coreLCenter.addBox(3.0F, 19.0F, 0.0F, 2, 1, 10, scale);
+        this.coreLCenter.setRotationPoint(0.0F, (float)b0, 0.0F);
+        this.coreRCenter = new ModelRenderer(this, 0, 0);
+        this.coreRCenter.addBox(3.0F, 19.0F, -2.0F, 2, 1, 10, scale);
+        this.coreRCenter.setRotationPoint(0.0F, (float)b0, 0.0F);
     }
     
     /**
      * Sets the models various rotation angles then renders the model.
      */
-    public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float p_78088_7_)
+    public void render(Entity p_78088_1_, float p_78088_2_, float p_78088_3_, float p_78088_4_, float p_78088_5_, float p_78088_6_, float par7)
     {
-        this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, p_78088_7_, p_78088_1_);
-        this.core.render(p_78088_7_);
-//        this.body.render(p_78088_7_);
-//        this.leg1.render(p_78088_7_);
-//        this.leg2.render(p_78088_7_);
-//        this.leg3.render(p_78088_7_);
-//        this.leg4.render(p_78088_7_);
+        this.setRotationAngles(p_78088_2_, p_78088_3_, p_78088_4_, p_78088_5_, p_78088_6_, par7, p_78088_1_);
+        this.core.render(par7);
+        this.coreLCenter.render(par7);
+        this.coreRCenter.render(par7);
     }
     
     /**
@@ -67,13 +55,11 @@ public class ModelBlob extends ModelBase {
      * and legs, where par1 represents the time(so that arms and legs swing back and forth) and par2 represents how
      * "far" arms and legs can swing at most.
      */
-    public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_)
+    public void setRotationAngles(float speed, float p_78087_2_, float time, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_)
     {
-        this.core.rotateAngleY = p_78087_4_ / (180F / (float)Math.PI);
-        this.core.rotateAngleX = p_78087_5_ / (180F / (float)Math.PI);
-//        this.leg1.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F) * 1.4F * p_78087_2_;
-//        this.leg2.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F + (float)Math.PI) * 1.4F * p_78087_2_;
-//        this.leg3.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F + (float)Math.PI) * 1.4F * p_78087_2_;
-//        this.leg4.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F) * 1.4F * p_78087_2_;
+    	  this.coreLCenter.offsetZ = MathHelper.cos(0.1F*time + 0.1F*speed)*0.05F;
+    	  this.coreRCenter.offsetZ = MathHelper.cos(0.3F*time + 0.3F*speed + .2F)*0.05F;
     }
+    
+
 }
