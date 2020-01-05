115d2e9c3ab0c1cd344eedc4d4b033b6c385e3dd
steef23 <59206556+steef23@users.noreply.github.com>
2020-01-05 22:10:29 +0100

    changed the way blob is rendered to allow it's model to be change while the game is running

--- main/java/com/yzarc/boss/render/RenderBlob.java
+++ main/java/com/yzarc/boss/render/RenderBlob.java
@@ -7,27 +7,50 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.ResourceLocation;
 
 import com.yzarc.boss.BossMain;
-import com.yzarc.boss.entity.EntityBlob;;
+import com.yzarc.boss.entity.EntityBlob;
+import com.yzarc.boss.model.ModelBlob;;
 
 @SideOnly(Side.CLIENT)
 public class RenderBlob extends RenderLiving {
 
     private static final ResourceLocation blobTextures = new ResourceLocation(BossMain.MODID + ":textures/entity/blob_base.png");
+    
+    protected ModelBlob entityModel;
 
-    public RenderBlob(ModelBase p_i1253_1_, float p_i1253_2_)
+    public RenderBlob(ModelBase modelBlob, float scale)
     {
-        super(p_i1253_1_, p_i1253_2_);
+        super(modelBlob, scale);
+        
+        entityModel = ((ModelBlob) mainModel);
+    }
+    
+    public void renderBlob(EntityBlob entity, double x, double y, double z, float u, float v)
+    {
+    	this.entityModel.setBig(entity.isBig());
+    	
+    	super.doRender(entity, x, y, z, u, v);
+    }
+    
+    public void doRenderLiving(EntityLiving entityLiving, double x, double y, double z, float u, float v)
+    {
+    	renderBlob((EntityBlob)entityLiving, x, y, z, u, v);
+    }
+    
+    public void doRender(Entity entity, double x, double y, double z, float u, float v)
+    {
+    	renderBlob((EntityBlob)entity, x, y, z, u, v);
     }
 
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
      */
-    protected ResourceLocation getEntityTexture(EntityBlob p_110775_1_)
+    protected ResourceLocation getEntityTexture(EntityBlob entity)
     {
         return blobTextures;
     }
@@ -35,9 +58,9 @@ public class RenderBlob extends RenderLiving {
     /**
      * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
      */
-    protected ResourceLocation getEntityTexture(Entity p_110775_1_)
+    protected ResourceLocation getEntityTexture(Entity entity)
     {
-        return this.getEntityTexture((EntityBlob)p_110775_1_);
+        return this.getEntityTexture((EntityBlob)entity);
     }
 }
 
