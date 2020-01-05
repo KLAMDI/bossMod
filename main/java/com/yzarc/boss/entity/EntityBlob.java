9b72668d3fe8a4c7dd51e0d59314d1669afa2fed
steef23 <59206556+steef23@users.noreply.github.com>
2020-01-05 15:11:07 +0100

    Added the isBig functionally when you get close to the entity

--- main/java/com/yzarc/boss/entity/EntityBlob.java
+++ main/java/com/yzarc/boss/entity/EntityBlob.java
@@ -1,7 +1,10 @@
 package com.yzarc.boss.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -13,12 +16,17 @@ import net.minecraft.world.World;
 
 public class EntityBlob extends EntityMob
 {
+	private float sizeY = 1.0F;
+	private float sizeX = 1.0F;
+	
+	private boolean isBig = false;
 	
 	public EntityBlob(World worldIn) 
 	{
 		super(worldIn);
-		this.setSize(1.5F,5.0F);
-		this.tasks.addTask(0, new EntityAIWander(this, 0.1D));
+		this.setSize(sizeY,sizeX);
+		//this.tasks.addTask(0, new EntityAIWander(this, 0.1D));
+		//this.tasks.addTask(1, new EntityAIChangeStance(this, EntityPlayer.class, 4));
 		//this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
 		//this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
 		
@@ -35,9 +43,16 @@ public class EntityBlob extends EntityMob
 	
 	public void onLivingUpdate()
 	{
+		changeStance();
 		super.onLivingUpdate();
 	}
-	
+
+	@Override
+	protected void updateEntityActionState() {
+		// TODO Auto-generated method stub
+		//super.updateEntityActionState();
+	}
+
 	protected Item getDropItem()
 	{
 		return Items.diamond_hoe;
@@ -47,4 +62,35 @@ public class EntityBlob extends EntityMob
 	{
 		return super.getCanSpawnHere();
 	}
+	
+	public void setSizeY(float sizeY)
+	{
+		this.sizeY = sizeY;
+	}
+	
+	public void setSizeX(float sizeX)
+	{
+		this.sizeX = sizeX;
+	}
+	
+    public void setSizeXY(float sizeY, float sizeX)
+    {
+    	this.setSizeX(sizeX);
+    	this.setSizeY(sizeY);
+    	this.setSize(sizeX, sizeY);
+    }
+    
+    public void changeStance()
+    {
+    	if (this.worldObj.getClosestPlayerToEntity(this, 8.0D) != null)
+    	{
+    		isBig = true;
+    		this.setSizeXY(2.0F, 2.0F);
+    	}
+    	else
+    	{
+    		isBig = false;
+    		this.setSizeXY(1.0F, 1.0F);
+    	}
+    }
 }
