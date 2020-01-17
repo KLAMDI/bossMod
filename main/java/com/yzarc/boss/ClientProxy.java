32bdb59e7c371ab8bd39cc5bc5c13758c84fb457
steef23 <59206556+steef23@users.noreply.github.com>
2020-01-17 20:29:25 +0100

    Added a creative tab named tabBossMod

--- main/java/com/yzarc/boss/ClientProxy.java
+++ main/java/com/yzarc/boss/ClientProxy.java
@@ -1,18 +1,29 @@
 package com.yzarc.boss;
 
+import com.yzarc.boss.blocks.BossBlocks;
 import com.yzarc.boss.render.BossRenderer;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
 public class ClientProxy extends CommonProxy 
 {
+    public static CreativeTabs bossModTab;
+    
 	@Override
 	public void preInit(FMLPreInitializationEvent e) {
-		super.preInit(e);
+		bossModTab = new CreativeTabs("tabBossMod") {
+			public Item getTabIconItem() 
+			{
+				return Item.getItemFromBlock(BossBlocks.test);
+			}
+		};
 		BossRenderer.init();
+		super.preInit(e);
 	}
 
 	@Override
