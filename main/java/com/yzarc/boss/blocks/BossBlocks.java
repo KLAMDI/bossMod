7dbdf3548c2084d833201b4fff39e69965448e33
KLAMDI <crunch@hetnet.nl>
2020-02-07 20:13:23 +0100

    Added slime trail block, currently basically stone colored snow

--- main/java/com/yzarc/boss/blocks/BossBlocks.java
+++ main/java/com/yzarc/boss/blocks/BossBlocks.java
@@ -11,9 +11,11 @@ import net.minecraft.creativetab.CreativeTabs;
 public class BossBlocks 
 {
     public static Block test;
+    public static Block BlockSlimeTrail;
     public static final void init() 
     {
     	GameRegistry.registerBlock(test = new CreateBlocks("test", Material.cake, ClientProxy.bossModTab, 2.0F, 6.0F, 1.0F, "pickaxe", 3, Block.soundTypeGravel), "test_block");
+    	GameRegistry.registerBlock(BlockSlimeTrail = new BlockSlimeTrail(), "slime_trail");
     }
     
 }
---
+++ main/java/com/yzarc/boss/blocks/BossMaterials.java
@@ -0,0 +1,16 @@
+package com.yzarc.boss.blocks;
+
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialLogic;
+
+public class BossMaterials extends Material {
+
+	public static final BossMaterials slime = (BossMaterials) (new Material(MapColor.foliageColor));
+	
+	public BossMaterials(MapColor p_i2116_1_) {
+		super(p_i2116_1_);
+
+	}
+
+}
