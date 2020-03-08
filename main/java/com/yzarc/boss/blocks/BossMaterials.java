920e88d6015a24967be8f54a00703d3067ba4797
KLAMDI <crunch@hetnet.nl>
2020-03-08 14:12:11 +0100

    Added slime material

--- main/java/com/yzarc/boss/blocks/BossMaterials.java
+++ main/java/com/yzarc/boss/blocks/BossMaterials.java
@@ -5,12 +5,20 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialLogic;
 
 public class BossMaterials extends Material {
-
-	public static final BossMaterials slime = (BossMaterials) (new Material(MapColor.foliageColor));
+	
+	public static final Material slime = (new Material(MapColor.foliageColor));
+   
+	/** The color index used to draw the blocks of this material on maps. */
+    private final MapColor materialMapColor;
 	
 	public BossMaterials(MapColor p_i2116_1_) {
 		super(p_i2116_1_);
-
+		this.materialMapColor = p_i2116_1_;
 	}
+	
+    public MapColor getMaterialMapColor()
+    {
+        return this.materialMapColor;
+    }
 
 }
